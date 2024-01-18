40ca1a3cff157e3bed12af84d6fa0540c65da21f
Vlone Me <francisntima@gmail.com>
2024-01-18 20:59:07 +0000

    Buy buy buy

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,15 +1,7 @@
 -- This reate a trigger to decrease the quantity of
--- an item after adding a new order
-DELIMITER //
-CREATE TRIGGER decrease_quantity_item
-AFTER INSERT ON orders
+-- an item after adding a new order.
+CREATE TRIGGER lower_quantity_item
+AFTER INSERT
+ON orders
 FOR EACH ROW
-BEGIN
-    -- This update the quantity of the corresponding item
-    -- in the items table
-    UPDATE items
-    SET quantity = quantity - 1
-    WHERE name = NEW.item_name;
-END;
-//
-DELIMITER ;
+UPDATE items SET quantity = quantity - NEW.number WHERE NAME = NEW.item_name;
