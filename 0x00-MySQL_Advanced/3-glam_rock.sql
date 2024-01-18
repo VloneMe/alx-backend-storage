10c5e1fa2d42828e47dc8edbc10b08c036a6c67e
Vlone Me <francisntima@gmail.com>
2024-01-18 10:45:14 +0000

    Old school band

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,14 +1,6 @@
 -- This list bands with Glam rock as main style, ranked by longevity
 SELECT
     band_name,
-    style,
-    CONCAT(
-        IF(split IS NOT NULL, split, 2022) - formed,
-        ' years'
-    ) AS lifespan
-FROM
-    metal_bands
-WHERE
-    style = 'Glam rock'
-ORDER BY
-    IF(split IS NOT NULL, split, 2022) - formed DESC;
+    COALESCE(split, 2022) - formed as lifespan FROM metal_bands
+WHERE style LIKE '%Glam rock%' 
+ORDER BY lifespan DESC;
