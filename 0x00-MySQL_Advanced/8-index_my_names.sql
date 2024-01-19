06d26d04cb7e7bbb3614c2e6909fba8069961e1c
vlone <vlone@localhost.localdomain>
2024-01-19 07:28:03 +0300

    Updated

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,3 @@
--- This create the index idx_name_first on the first letter of the name column
-CREATE INDEX idx_name_first ON names (SUBSTRING(name, 1, 1));
+-- This create the index idx_name_first on
+-- the first letter of the name column
+CREATE INDEX idx_name_first ON names(name(1));
