188946a6297debe8aa5e0b8d84bf9fc12bb926f7
Vlone Me <francisntima@gmail.com>
2024-01-25 01:53:57 +0000

    Implementing an expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,9 +5,10 @@ import requests
 from functools import wraps
 from typing import Callable
 
+
 def track_and_cache_page(fn: Callable) -> Callable:
     """
-    Decorator for tracking and caching the result of the get_page function.
+    The decorator for tracking and caching the result of the get_page function.
 
     Args:
         fn (Callable): The function to be decorated.
@@ -18,7 +19,7 @@ def track_and_cache_page(fn: Callable) -> Callable:
     @wraps(fn)
     def wrapper(url: str) -> str:
         """
-        Wrapper function that:
+        The wrapper function that:
         - checks whether a URL's data is cached
         - tracks the usage count of the decorated function
 
@@ -31,26 +32,27 @@ def track_and_cache_page(fn: Callable) -> Callable:
         redis_client = redis.Redis()
         redis_key_count = f'count:{url}'
         redis_key_cached_data = f'cached_data:{url}'
-        
-        # Track usage count
+
+        """ Track usage count"""
         redis_client.incr(redis_key_count)
 
-        # Check if data is cached
+        """ Check if data is cached"""
         cached_data = redis_client.get(redis_key_cached_data)
         if cached_data:
             return cached_data.decode('utf-8')
 
-        # Fetch data and cache it
+        """ Fetch data and cache it"""
         response = fn(url)
         redis_client.setex(redis_key_cached_data, 10, response)
         return response
 
     return wrapper
 
+
 @track_and_cache_page
 def get_page(url: str) -> str:
     """
-    Makes an HTTP request to a given URL.
+    This akes an HTTP request to a given URL.
 
     Args:
         url (str): The URL to be accessed.
